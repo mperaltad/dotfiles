4eb320ee29b168e3f1520f7d6e216afbc38f3a68
Manu-Peralta <manuperalta@skyscanner.net>
2024-04-04 17:21:31 +0200

    Adapted configs for Mac

--- dot_local/share/private_cinnamon/applets/Sensors@claudiux/scripts/executable_get_distro.js
+++
@@ -1,5 +0,0 @@
-const osinfo = imports.gi.Libosinfo;
-
-let osInfo = new osinfo.Os();
-
-global.log(osInfo.get_distro());
--- dot_local/share/private_cinnamon/applets/weather@mockturtl/3.8/executable_applet.js
+++
@@ -1,5 +0,0 @@
-const { weatherApplet } = require('./weather-applet');
-
-function main(metadata, orientation, panel_height, instance_id) {
-    return weatherApplet.main(metadata, orientation, panel_height, instance_id);
-}
