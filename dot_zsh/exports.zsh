07efb31eacc4e4f9520ab67657e2bd555d97f3a6
Manu-Peralta <manuperalta@skyscanner.net>
2024-05-13 16:09:44 +0200

    Updated aliases for reloading tmux and zsh config + enabled pyenv virtualenv support

--- dot_zsh/exports.zsh
+++ dot_zsh/exports.zsh
@@ -2,3 +2,7 @@
 #export PIP_REQUIRE_VIRTUALENV=true
 # cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+export PYENV_ROOT="$HOME/.pyenv"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=0
