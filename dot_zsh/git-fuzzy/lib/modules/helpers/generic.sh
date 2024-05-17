4dc189ef4b6ddd7ca97a398379d7bc82e6fd4ff8
Manu-Peralta <manuperalta@skyscanner.net>
2024-05-17 17:57:59 +0200

    Removed git-fuzzy repo. Moved zsh_history from symlink to cloud copy directly because of bug in zsh zsh_history config that overrides symlinks

--- dot_zsh/git-fuzzy/lib/modules/helpers/generic.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-gf_fzf_log_line_interpreter() {
-  cat - | awk '{ print $1 }'
-}
--- dot_zsh/zsh.zsh
+++
@@ -1,9 +0,0 @@
-export COMPLETION_WAITING_DOTS="true"
-
-# export HISTSIZE=32768;
-# export HISTFILESIZE=$HISTSIZE;
-export HISTCONTROL=ignoredups:erasedups;
-export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help";
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_SAVE_NO_DUPS
