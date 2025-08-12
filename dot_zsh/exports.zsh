53a16f7d4edc707a0b98cb0e423ab31f7a8b2d15
mperaltad <manuchorima@gmail.com>
2025-08-12 15:26:59 +0200

    snapshot August refactors

--- dot_zsh/exports.zsh
+++ dot_zsh/exports.zsh
@@ -1,9 +1,8 @@
 # pip should only run if there is a virtualenv currently activated
-#export PIP_REQUIRE_VIRTUALENV=true
+export PIP_REQUIRE_VIRTUALENV=true
 # cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=0
-export HOMEBREW_NO_ENV_HINTS=1
