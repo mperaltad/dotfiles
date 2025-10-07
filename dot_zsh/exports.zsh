fbaee194ec0a45f46279e174096764928789aa22
manuel.peralta <manuel.peralta@immfly.com>
2025-10-07 11:42:49 +0200

    Merging mac & Linux configs

--- dot_zsh/exports.zsh
+++ dot_zsh/exports.zsh
@@ -2,7 +2,13 @@
 export PIP_REQUIRE_VIRTUALENV=true
 # cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+
+export PYPI_USERNAME="__TOKEN__"
+export PYPI_TOKEN=""
+
 export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+# Automatically activate/deactivate virtualenvs on entering/leaving directories containing .python-version 
 eval "$(pyenv virtualenv-init -)"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=0
