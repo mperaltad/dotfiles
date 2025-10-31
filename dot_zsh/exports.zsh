c1f13b2e07e0eb55c240cd5d119b4e05432d9e9f
manuel.peralta <manuel.peralta@immfly.com>
2025-10-31 09:40:39 +0100

    MacOS improvements for work

--- dot_zsh/exports.zsh
+++ dot_zsh/exports.zsh
@@ -9,6 +9,4 @@ export PYPI_TOKEN=""
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-# Automatically activate/deactivate virtualenvs on entering/leaving directories containing .python-version 
 eval "$(pyenv virtualenv-init -)"
-export PYENV_VIRTUALENV_DISABLE_PROMPT=0
